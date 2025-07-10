c966b4fb3bd0c223180d33d5d1c4c681ca344f1e
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-07-11 00:05:49 +0700

    .

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-import ClientLayoutWrapper from "@/components/client/layout/ClientLayoutWrapper"; // 👈 Tách wrapper
+
 
 const inter = Inter({ subsets: ["latin"] });
 
